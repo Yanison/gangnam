6fdeb44ead3ef64bda73977e23a90d06bc498c57
seungjun12 <vndan110@naver.com>
2022-10-26 17:22:27 +0900

    유저 컨트롤러 수정

--- src/main/java/com/sgworld/infra/modules/user/board/BoardController.java
+++ src/main/java/com/sgworld/infra/modules/user/board/BoardController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
 	
 	//계시판 리스트
-	@RequestMapping(value="/boardList")
+	@RequestMapping(value="boardList")
 	public String boardList() {
 		return "infra/user/modules/board/boardHome";
 	}
 	
 	//계시판 글쓰기
-	@RequestMapping(value="/boardWrite")
+	@RequestMapping(value="boardWrite")
 	public String boardWrite() {
 		return "infra/user/modules/board/boardRegForm";
 	}
