a65647b4bde5e05a7fecf25981607eed34deb54e
Jiwon93 <ls_sjw93@naver.com>
2022-11-24 14:25:36 +0900

    commit

--- src/main/java/com/sgworld/infra/common/interceptor/CheckLoginSessionInterception.java
+++ src/main/java/com/sgworld/infra/common/interceptor/CheckLoginSessionInterception.java
@@ -2,7 +2,6 @@ package com.sgworld.infra.common.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
