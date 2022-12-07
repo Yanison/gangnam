69cd36e3067f8db861bb99315f3c302eef5981f3
ghkdtjsdi@naver.com <ghkdtjsdi@naver.com>
2022-12-07 18:02:40 +0900

    20221207 boardview 페이징 수정, home 자유게시판 리스트 추가

--- src/main/java/com/sgworld/infra/modules/user/board/BoardController.java
+++ src/main/java/com/sgworld/infra/modules/user/board/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -43,8 +44,9 @@ public class BoardController {
 	}
 	
 	//게시글 보기
-	@RequestMapping(value = "boardView")
-	public String boardView(@ModelAttribute("vo") AdminBoardVo vo, AdminBoardDto dto, Model model) throws Exception {
+	@RequestMapping(value = "boardView/{bdSeq}")
+	public String boardView(@ModelAttribute("vo") AdminBoardVo vo, AdminBoardDto dto, Model model,
+				@PathVariable String bdSeq) throws Exception {
 		
 		service.boardViewCount(dto);
 		AdminBoardDto like = service.boardLiked(vo);
